109b511af728d691f8a05343866632c93a1aadf4
Melody0304 <yuqing.ma@edu.SAIT.ca>
2023-04-22 07:55:15 +0800

    添加项目文件。

---
+++ connect/Program.cs
@@ -0,0 +1,26 @@
+﻿using MySqlConnector;
+
+namespace connect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder() 
+            { 
+                Server = "127.0.01",
+                UserID = "root",
+                Password = "password",
+                Database = "demo211",
+            };
+            
+            MySqlConnection connection = new MySqlConnection(builder.ConnectionString);
+
+            connection.Open();
+
+            Console.WriteLine("Connected to database.");
+
+            connection.Close();
+        }
+    }
+}
